c81ff984ae5c1344dfb0f29f8264706c0dc8d251
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2025-11-28 11:45:19 +0800

    init commit

---
+++ src/main/java/com/stack_advisor/stack_advisor_backend/StackAdvisorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stack_advisor.stack_advisor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackAdvisorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackAdvisorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stack_advisor/stack_advisor_backend/StackAdvisorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stack_advisor.stack_advisor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackAdvisorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
